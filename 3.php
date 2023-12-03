9364f9f8cc01bcbc22e8adbebc02fb10cb5a2111
James Wallace <sausages@gail.com>
2023-12-03 08:29:10 -0500

    First few attempts, not workin yet

---
+++ 3.php
@@ -0,0 +1,66 @@
+#!/usr/bin/php
+<?php
+// return the sum of all numbers adjacent to a symbol
+
+// 528309 is too high
+// 520386 is too high
+
+$lines = file("input3.txt");
+
+$sum = 0;
+
+for ($linenum = 0; $linenum < sizeof($lines); $linenum++) {
+	// iterate line by line, looking for numbers
+	$line = $lines[$linenum];
+
+	if (preg_match_all("@[0-9]+@", $line, $matches)) {
+		if (!empty($matches[0])) {
+			$offset = 0;
+
+			foreach ($matches[0] as $num) {
+				$pos = strpos($line, $num, $offset);
+
+				if ($pos !== false) {
+					$offset = $pos;
+	
+					$coords = array();
+
+					if ($pos - 1 > 0) {
+						$coords[][$linenum] = $pos - 1;
+					}
+
+					if (($pos + strlen($num)) <= strlen($line)) {
+						$coords[][$linenum] = $pos + strlen($num);
+					}
+
+					for ($y = -1; $y <= strlen($num); $y++) {
+						if ($linenum != 0 && ($y + $pos > 0) && ($y + $pos) <= strlen($line)) {
+							$coords[][$linenum - 1] = $y + $pos;
+						}
+
+						if ($linenum < sizeof($lines) && ($y + $pos > 0) && ($y + $pos) <= strlen($line)) {
+							$coords[][$linenum + 1] = $y + $pos;
+						}
+					}
+					
+					// now lets search the coords for a symbol!
+					foreach ($coords as $coord) {
+						foreach ($coord as $x => $y) {
+							if ($y == 140) {
+								continue;
+							}
+
+							if (preg_match("@[^0-9\.]@", substr($lines[$x], $y, 1))) {
+								// we found a valid part number to be added to the sum
+								$sum += (int)$num;
+								break 2;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+echo "Got a sum of $sum\n";
