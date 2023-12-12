23d2200923bb7ec1ae892d5cfc4efd505aee8ae9
James Wallace <sausages@gail.com>
2023-12-12 07:06:50 -0500

    Finished 12a

---
+++ 12.php
@@ -0,0 +1,95 @@
+#!/usr/bin/php
+<?php
+
+/// the correct answer was 7506
+// . operational
+// # damaged
+// ? unknown
+
+// the numbers represent the series of contiguous springs
+
+// how many different arrangements of operational and broken sprngs
+// fit the given criteria in each row?
+
+$lines = file("input12.txt");
+$sum = 0;
+
+foreach ($lines as $line) {
+	$bits = explode(" ", trim($line));
+
+	$pattern = $bits[0];
+	$contiguous = $bits[1];
+
+	echo "$pattern   $contiguous\n";
+
+	// get the positions in the pattern where a ? is located
+	$split_pattern = str_split($pattern);
+	$fills = array();
+
+	for ($x = 0; $x < count($split_pattern); $x++) {
+		if ($split_pattern[$x] == "?") {
+			$fills[] = $x;
+		}
+	}
+
+	//echo "$pattern\n";
+	//print_r($fills);
+	$binary_max = pow(2, count($fills));
+	echo "There are 2^" . count($fills) . " possibilities here ($binary_max)\n";
+
+	$wayCount = 0;
+
+	for ($x = 0; $x < $binary_max; $x++) {
+		$b = str_pad(decbin($x), count($fills), "0", STR_PAD_LEFT);
+		$c = str_replace("1", "#", str_replace("0", ".", $b));
+
+		// plug it into the main string
+		$d = str_split($c);
+
+		for ($y = 0; $y < count($fills); $y++) {
+			$split_pattern[$fills[$y]] = $d[$y];		
+		}
+
+		$r = explode(",", $contiguous);
+		$t = implode("", $split_pattern);
+
+		$pat = convertPattern($t);
+		echo "Converting $t gets us " . implode(",", $pat) . "\n";
+		if (convertPattern($t) == $r) {
+			echo "Got a match!\n";
+			$wayCount++;
+		}
+	}
+
+	$sum += $wayCount;	
+}
+
+echo "We have a total arrangement count of $sum\n";
+
+function convertPattern($pattern) {
+	// $pattern should contain only # or .
+	// #.#.### = 1,1,3
+	$ret = array();
+
+	$bits = str_split($pattern);
+
+	$curCount = 0;
+
+	for ($x = 0; $x < count($bits); $x++) {
+		if ($bits[$x] == "#") {
+			$curCount++;
+		} else if ($bits[$x] == ".") {
+			if ($curCount > 0) {
+				$ret[] = $curCount;
+			}
+
+			$curCount = 0;
+		}
+	}
+
+	if ($curCount > 0) {
+		$ret[] = $curCount;
+	}
+
+	return $ret;
+}
