db8c2c09fa2cbf09545cb1867366c0041899a435
James Wallace <sausages@gail.com>
2023-12-13 13:33:50 -0500

    Still haven't got 13b, but now I know a RANGE

--- 13b.php
+++ 13b.php
@@ -3,7 +3,10 @@
 // ash .
 // rocks #
 
-$lines = file("input13-sample.txt");
+// 28427 is too low.
+// 34127 is too high.
+
+$lines = file("input13.txt");
 
 $maps = array();
 $map = array();
@@ -78,6 +81,56 @@ foreach ($maps as $map) {
 
 	// now we have our firstmatch, and the details of it are in $firstMatch
 	// lets go through again, looking for a line thats 1 off a match
+	$found = false;
+
+	// find a reflection either horizontally
+	for ($x = 0; $x < count($map) - 1; $x++) {
+		if (trim($map[$x]) == trim($map[$x + 1])) {
+			//echo "Potentially found a mirror horizontally between $x and " . ($x + 1) . "\n";
+
+			if ($firstMatch == "H$x") {
+				continue;
+			}
+
+			// we need to make sure it's a one away mirror
+			if (isMirroredHorizontallyAlmost($x, $map)) {
+				echo "Mirrored horizontally almost at $x - " . ($x + 1) . "\n";
+				$found = true;
+				$sum += 100 * ($x + 1);
+
+				break;
+			}
+		}	
+	}
+
+	// or vertically
+	if (!$found) {
+		$map = shiftMapVertically($map);
+
+		//echo "Shifting 90d\n";
+		//printArray($map);
+
+		// find a reflection either horizontally
+		for ($x = 0; $x < count($map) - 1; $x++) {
+			if (trim($map[$x]) == trim($map[$x + 1])) {
+				//echo "Potentially found a mirror vertically between $x and " . ($x + 1) . "\n";
+
+				if ($firstMatch == "V$x") {
+					continue;
+				}
+
+				// we need to make sure it's a perfect mirror
+				if (isMirroredHorizontallyAlmost($x, $map)) {
+					echo "Mirrored vertically almost at $x - " . ($x + 1) . "\n";
+					$found = true;
+
+					$sum += $x + 1;
+
+					break;
+				}
+			}	
+		}
+	}
 
 }
 
@@ -179,5 +232,41 @@ function isMirroredHorizontally($upTo, $map) {
 	}
 
 	echo "We have a confirmed mirror.\n";
+	return true;
+}
+
+function isMirroredHorizontallyAlmost($upTo, $map) {
+	$haveOne = false; // whether we've already encountered the off by one line	
+
+	$y = $upTo + 1;
+
+	for ($x = $upTo; $x >= 0; $x--) {
+		// lets loop the lines, looking for all 0's except for 1 x offby1
+		if (!isset($map[$y])) {
+			continue;
+		}
+
+		$offBy = offBy($map[$x], $map[$y]);
+
+		if ($offBy > 1) {
+			// this isn't it
+			echo "We have an offby > 1, not it\n";
+
+			return false;
+		} else if ($offBy == 1) {
+			if ($haveOne) {
+				echo "We have more than 1 offby, not it\n";
+
+				return false;
+			}
+
+			$haveOne = true;
+		} 
+
+		$y++;
+	}
+
+	echo "We found it!\n";
+
 	return true;
 }
