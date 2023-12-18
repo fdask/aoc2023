63bbb0ce5f72c04664949dbc412908201edfde1d
James Wallace <sausages@gail.com>
2023-12-18 06:10:49 -0500

    Started 18.
    Really not sure how this would be contained.  If we use an array, we may have to
    go up, which could lead to negative indexes and weirdness.

---
+++ 18.php
@@ -0,0 +1,62 @@
+#!/usr/bin/php
+<?php
+
+$lines = file("input18-sample2.txt");
+
+// starts in a 1 meter cube hole in the ground
+// dig the specified number of meters u/d/l/r (north/south/west/east)
+
+// each trench is also listed with the color that the edge of the trench should be painted
+// as an rgb hexidecimal code
+
+// this trench should form a loop
+
+// dig out the center of the loop
+
+// how many cubic meters of lava could the dug out trench hold?
+$map = array();
+
+$posX = 0;
+$posY = 0;
+
+$map[0][0] = "#";
+
+foreach ($lines as $line) {
+	if (preg_match("@([ULDR]) ([\d+]) \((#[a-z0-9]{6})\)@", trim($line), $matches)) {
+		$direction = $matches[1];
+		$meters = $matches[2];
+		$color = $matches[3];
+
+		echo "Dig $meters $direction, ($color)\n";
+
+		switch ($direction) {
+			case 'D':
+				echo "Starting at posX ($posX, $posY) we go..";
+
+				for ($x = $posX + 1; $x <= ($posX + $meters); $x++) {
+					echo "($x, $posY) ";
+	
+					if (!isset($map[$x])) {
+						$map[$x] = array();
+					}
+
+					$map[$x][$posY] = "#";
+				}
+
+				$posX = $x;
+
+				echo "\n";
+
+				break;
+			case 'U':
+				break;
+			case 'R':
+				break;
+			case 'L':
+				break;
+			default:
+				echo "Shouldn't be here A\n";
+				exit;
+		}
+	}
+}
